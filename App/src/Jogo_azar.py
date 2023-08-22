d9650e765c1cdaa6519db99e5f522f6e6688daf2
leoCaversan <leocaversan7@gmail.com>
2023-08-21 22:47:16 -0300

    fix: generete new response creating new router

--- App/src/Jogo_azar.py
+++ App/src/Jogo_azar.py
@@ -1,26 +1,32 @@
 import random
 
+
+class JogoAzarManager:
+    def __init__(self):
+        self.resposta = Jogo_azar.gera_resposta()
+    def gera_nova_resposta(self):
+        self.resposta = Jogo_azar.gera_resposta()
+
 class Jogo_azar():
 
     def __init__(self):
         '''O jogo gera um numero aleatorio e vai dando dicas até que acerte.'''
         
     def main(chute, response):
-        resposta = int(response)
         chute_ = int(chute)
+        resposta = int(response)
         
         if chute_ > resposta:
-            return (f"Errou! É um valor menor que {chute}")
+            return f"Errou! É um valor menor que {chute}"
         elif chute_ < resposta:
-            return (f"Errou! É um valor maior que {chute}")
+            return f"Errou! É um valor maior que {chute}"
         else:
-            return (f"Parabéns! O número gerado foi {response}")
+            return f"Parabéns! O número gerado foi {response}"
 
     
     def gera_resposta():
         resposta = random.randint(1,9)
         return resposta
 
-if __name__ == '__main__':    
-    Jogo_azar()
+
     
--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from App.src.Jogo_azar import Jogo_azar
+from App.src.Jogo_azar import Jogo_azar, JogoAzarManager
 from flask import Flask, request
 
 app = Flask(__name__)
 
-resposta = Jogo_azar.gera_resposta()
+
+jogo_manager = JogoAzarManager()
 
 @app.post("/jogo_azar")
 def game_bad_luck():
     chute = request.json.get('chute')
-    result = Jogo_azar.main(chute, resposta)
-    return  {
-                "resultado":(result)
-            }
-        
-@app.route("/")
-def hello_world():
+    result = Jogo_azar.main(chute, jogo_manager.resposta)
     return {
-        "username": "teste"
-    }
+                "resultado": result
+            }
+
+@app.get("/gera_nova_resposta")
+def generate_new_response():
+    resposta = jogo_manager.gera_nova_resposta()
+    return {
+                "message": "Novo valor gerado com sucesso!", 
+            }
+
 @app.errorhandler(404)
 def resource_not_found(e):
     return {
